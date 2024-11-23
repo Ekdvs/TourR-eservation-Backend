08bc85c6e0b40f65b5d2a0269edfceb6d9351f0e
Ekdvs <ekdvsampath02@gmail.com>
2024-11-23 20:11:47 +0530

    error fix

--- src/main/java/com/travel/travelPlanningSite_Backend/Service/UserService.java
+++ src/main/java/com/travel/travelPlanningSite_Backend/Service/UserService.java
@@ -3,7 +3,7 @@ package com.travel.travelPlanningSite_Backend.Service;
 import com.travel.travelPlanningSite_Backend.Model.User;
 
 import java.util.List;
-import java.util.Optional
+import java.util.Optional;
 
 public interface UserService {
     User saveUser(User user);
