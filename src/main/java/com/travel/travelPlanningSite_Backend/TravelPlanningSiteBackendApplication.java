f526e9ae1bae0878cc4fc319949be45bf22052d7
Ekdvs <ekdvsampath02@gmail.com>
2024-11-23 18:48:46 +0530

    create spring dendence

---
+++ src/main/java/com/travel/travelPlanningSite_Backend/TravelPlanningSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelPlanningSite_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanningSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlanningSiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/travelPlanningSite_Backend/TravelPlanningSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travelPlanningSite_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanningSiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
